53e483dfe50300a5525e7d27bde114a67551f861
SK207_23 <SK207_23@K207_W823.carrollcc.edu>
2016-04-04 20:35:57 -0400

    hooking game class to controller

---
+++ TicTacToe/src/edu/cis232/Board.java
@@ -0,0 +1,28 @@
+package edu.cis232;
+
+public class Board {
+	/*public String[][] board = new String[3][3];
+	
+	public boolean checkWin(){
+		String winner = null;
+		//check rows
+		winner = checkRowWinner();
+	}
+	
+	private String checkRowWinner(){
+		for(int row=0; row < board.length; row++){
+			boolean hasNull = false;
+			for(int col = 0; col < board[row].length; col++){
+				//check if there is a null first
+				if(board[row][col] == null){
+					hasNull = true;
+				}				
+			}
+			boolean hasDifferent = false;
+			String player = board[row][0];
+			if(!hasNull){
+				
+			}
+		}
+	}*/
+}
