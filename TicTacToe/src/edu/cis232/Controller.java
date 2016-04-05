603f6a401914c6c43579b475471ea53f885902e3
SK207_23 <SK207_23@K207_W823.carrollcc.edu>
2016-04-04 20:19:32 -0400

    test

--- TicTacToe/src/edu/cis232/Controller.java
+++ TicTacToe/src/edu/cis232/Controller.java
@@ -43,14 +43,15 @@ public class Controller {
     @FXML
     private Label output;
 
-    Image X = new Image("X.jpg");
+    Image X = new Image("http://static.saatchi.com/uploads/138737718027148/resize_then_crop_500_350.jpg");
+    Image O = new Image("https://openclipart.org/image/2400px/svg_to_png/87463/Cyrillic-O.png");
     
     @FXML
     void makeMove(ActionEvent event) {
     	Object source = event.getSource();
     	if(source instanceof ImageView){
     		ImageView Image = (ImageView) source;
-    		Image.setImage(X);
+    		Image.setImage(O);
     	}
     }
 }
