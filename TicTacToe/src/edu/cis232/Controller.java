ed5014dd535b59304dab5e139e01a4c0979ba3e2
SK207_23 <SK207_23@K207_W823.carrollcc.edu>
2016-04-04 20:13:07 -0400

    Created Controller and other things

--- TicTacToe/src/edu/cis232/Controller.java
+++ TicTacToe/src/edu/cis232/Controller.java
@@ -1,5 +1,56 @@
 package edu.cis232;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+public class Controller {
+    @FXML
+    private ImageView TL;
+
+    @FXML
+    private ImageView TM;
+
+    @FXML
+    private ImageView TR;
+
+    @FXML
+    private ImageView ML;
+
+    @FXML
+    private ImageView MM;
+
+    @FXML
+    private ImageView MR;
+
+    @FXML
+    private ImageView BL;
+
+    @FXML
+    private ImageView BM;
+
+    @FXML
+    private ImageView BR;
+
+    @FXML
+    private ImageView PlayerImg;
+
+    @FXML
+    private Label PlayerSelect;
+
+    @FXML
+    private Label output;
+
+    Image X = new Image("X.jpg");
+    
+    @FXML
+    void makeMove(ActionEvent event) {
+    	Object source = event.getSource();
+    	if(source instanceof ImageView){
+    		ImageView Image = (ImageView) source;
+    		Image.setImage(X);
+    	}
+    }
 }
